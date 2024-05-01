046e98a5279d34d7a0d81500583210e5adbdcee1
sreynuch17 <sreynuch.thoeun@student.passerellesnumeriques.org>
2024-05-01 10:16:09 +0700

    #19 class_meal

---
+++ src/Meal.ts
@@ -0,0 +1,30 @@
+enum typeOfMeals{
+    baby = "baby",
+    bland = "bland",
+    child = "child",
+    toddier = "toddier",
+    diabetic = "diabetic",
+    gluten_free = "gluten_free",
+    low_sodium_noSalt = "low_sodium_noSalt",
+    halai = "halai",
+    hindu = "hindu",
+}
+const meal1 = typeOfMeals.baby;
+const meal2 = typeOfMeals.bland;
+const meal3 = typeOfMeals.child;
+const meal4 = typeOfMeals.toddier;
+const meal5 = typeOfMeals.diabetic;
+const meal6 = typeOfMeals.gluten_free;
+const meal7 = typeOfMeals.low_sodium_noSalt;
+const meal8 = typeOfMeals.halai;
+const meal9 = typeOfMeals.hindu;
+
+console.log(meal1);
+console.log(meal2);
+console.log(meal3);
+console.log(meal4);
+console.log(meal5);
+console.log(meal6);
+console.log(meal7);
+console.log(meal8);
+console.log(meal9);
