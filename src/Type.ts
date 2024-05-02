02c784154a0ad9b17a6699fe6ea82041e6d6191d
sreynuch17 <sreynuch.thoeun@student.passerellesnumeriques.org>
2024-05-02 15:39:49 +0700

    #24 class_type

---
+++ src/Type.ts
@@ -0,0 +1,13 @@
+enum Types{
+    classes = "classes",
+    flex = "flex",
+    business = "business"
+}
+
+const type1 = Types.classes;
+const type2 = Types.flex;
+const type3 = Types.business;
+
+console.log(type1);
+console.log(type2);
+console.log(type3);
